61eb168df677d6a15ef7b8e4d169a908dcc5e581
oscar <46996403c@iespoblenou.org>
2016-01-15 17:52:14 +0100

    Muevo la activiti del perfil al sitio correspondiente

--- app/src/main/java/oscarxiii/xiii_game/ScrollingActivityProfile.java
+++ app/src/main/java/oscarxiii/xiii_game/ScrollingActivityProfile.java
@@ -1,4 +1,4 @@
-package oscarxiii.xiii_game.json.SteamPlayerAPI;
+package oscarxiii.xiii_game;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,8 +6,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
-import oscarxiii.xiii_game.R;
 
 public class ScrollingActivityProfile extends AppCompatActivity {
 
