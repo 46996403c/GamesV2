80043f38ff96313c00c00e7b68dacfd0c18728d7
oscar <46996403c@iespoblenou.org>
2016-01-19 15:26:53 +0100

    Añado las clases del JSON para ver los juegos comprados

---
+++ app/src/main/java/oscarxiii/xiii_game/json/SteamJuegosCompradosAPI/SteamJuegosComprados.java
@@ -0,0 +1,34 @@
+
+package oscarxiii.xiii_game.json.SteamJuegosCompradosAPI;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class SteamJuegosComprados {
+
+    @SerializedName("response")
+    @Expose
+    private Response response;
+
+    /**
+     *
+     * @return
+     * The response
+     */
+    public Response getResponse() {
+        return response;
+    }
+
+    /**
+     *
+     * @param response
+     * The response
+     */
+    public void setResponse(Response response) {
+        this.response = response;
+    }
+
+}
